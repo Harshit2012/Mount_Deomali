d77da98bdf8f3cf15aedf3b99c45e4f62ee9924c
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2023-05-24 20:55:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeImg(){
+    document.getElementById("logo_img").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfluzcdfLG376zO4mfNteJ9SyXVeSNrcIPeoK98MZWrw&usqp=CAU&ec=48600113"
+}
+
+function normalImg(){
+    document.getElementById("logo_img").src="logo.png"
+}
